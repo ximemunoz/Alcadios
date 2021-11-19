39c596dd431d2122cfa72f8a30edc59fc552cd38
Mario A <0235810@up.edu.mx>
2021-11-18 18:14:00 -0600

    olad

--- ola.py
+++ ola.py
@@ -1,2 +1,10 @@
-import math
-dificultad = 0
+from random import randit
+jugador = 1
+dificil = 0
+
+print ("Dificultad jugador 1, 1-100")
+dificil1= input ()
+print ("Dificultad jugador 2, 1-100")
+dificil2= input ()
+
+if 
